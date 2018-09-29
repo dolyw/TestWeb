f1dcbdf4acfbd8c6a7c5d8f1b54718e647ded397
随心 <1107224733@qq.com>
2018-09-29 16:58:49 +0800

    JavaMailUtil工具类新增TLS通信

--- src/main/java/com/wang/util/JavaMailUtil.java
+++ src/main/java/com/wang/util/JavaMailUtil.java
@@ -26,9 +26,21 @@ public class JavaMailUtil {
     private final static String SEMICOLON = ";";
 
     /**
-     * 发送邮件，通过qq邮件发送，因为具有ssl加密，采用的是smtp协议
-     * @param mailHost 邮件服务器的主机名 "smtp.qq.com"
-	 * @param loginAccount 登录邮箱的账号 "xxxxxxxx@qq.com"
+     * SSL
+     */
+    private final static String SSL = "SSL";
+
+    /**
+     * TLS
+     */
+    private final static String TLS = "TLS";
+
+    /**
+     * 发送邮件，通过OutLook邮件发送，因为具有TLS加密，采用的是SMTP协议
+     * 发送邮件，通过QQ邮件发送，因为具有SSH加密，采用的是SMTP协议
+     * @param mailHost 邮件服务器的主机名 "smtp-mail.outlook.com" "smtp.qq.com"
+     * @param type 类型 SSL or TLS
+	 * @param loginAccount 登录邮箱的账号 "xxxxxxxx@outlook.com" "xxxxxxxx@qq.com"
 	 * @param loginAuthCode 登录邮箱时候需要的密码(授权码)
 	 * @param name 昵称
 	 * @param recipients 收件人
@@ -38,22 +50,31 @@ public class JavaMailUtil {
      * @author Wang926454
      * @date 2018/9/14 16:35
      */
-    public static boolean sendEmail(String mailHost, final String loginAccount, final String loginAuthCode,
+    public static boolean sendEmail(final String mailHost, final String type, final String loginAccount, final String loginAuthCode,
                                     String name, String[] recipients, String mailSubject, String mailContent){
         try {
             // 跟smtp服务器建立一个连接
             Properties properties = new Properties();
             // 设置邮件服务器主机名
             properties.setProperty("mail.host", mailHost);
+            // properties.setProperty("mail.smtp.port", "587");
             // 发送服务器需要身份验证，要采用指定用户名密码的方式去认证
             properties.setProperty("mail.smtp.auth", "true");
             // 发送邮件协议名称
             properties.setProperty("mail.transport.protocol", "smtp");
-            //开启SSL加密，否则会失败
-            MailSSLSocketFactory mailSSLSocketFactory = new MailSSLSocketFactory();
-            mailSSLSocketFactory.setTrustAllHosts(true);
-            properties.put("mail.smtp.ssl.enable", "true");
-            properties.put("mail.smtp.ssl.socketFactory", mailSSLSocketFactory);
+            // 判断类型是SSL还是TLS
+            if(TLS.equals(type)){
+                // 开启TLS加密，否则会失败
+                properties.put("mail.smtp.starttls.enable", "true");
+            }else if(SSL.equals(type)){
+                // 开启SSL加密，否则会失败
+                MailSSLSocketFactory mailSSLSocketFactory = new MailSSLSocketFactory();
+                mailSSLSocketFactory.setTrustAllHosts(true);
+                properties.put("mail.smtp.ssl.enable", "true");
+                properties.put("mail.smtp.ssl.socketFactory", mailSSLSocketFactory);
+                // 不做服务器证书校验
+                // properties.put("mail.smtp.ssl.checkserveridentity", "false");
+            }
             // 创建session
             Session session = Session.getDefaultInstance(properties,new Authenticator(){
                 @Override
@@ -97,7 +118,7 @@ public class JavaMailUtil {
             Transport.send(message);
             System.out.println("邮件发送成功");
         } catch (GeneralSecurityException e){
-            System.out.println("邮件发送失败: "+e.getMessage());
+            System.out.println("邮件发送失败: " + e.getMessage());
             return false;
         } catch (MessagingException e) {
             System.out.println("邮件发送失败: "+e.getMessage());
@@ -106,7 +127,13 @@ public class JavaMailUtil {
         return true;
     }
 
-    public static boolean sendEmail(String mailHost, final String loginAccount, final String loginAuthCode,
+    /**
+     * 方法重载，收件人以String方式传递，逗号或分号分割
+     * @return boolean
+     * @author Wang926454
+     * @date 2018/9/29 16:12
+     */
+    public static boolean sendEmail(final String mailHost, final String type, final String loginAccount, final String loginAuthCode,
                                     String name, String recipients, String mailSubject, String mailContent){
         String[] recipientsTemp;
         if(recipients.indexOf(COMMA) != -1){
@@ -117,17 +144,17 @@ public class JavaMailUtil {
             recipientsTemp = new String[1];
             recipientsTemp[0] = recipients;
         }
-        return JavaMailUtil.sendEmail(mailHost, loginAccount, loginAuthCode, name, recipientsTemp, mailSubject, mailContent);
+        return JavaMailUtil.sendEmail(mailHost, type, loginAccount, loginAuthCode, name, recipientsTemp, mailSubject, mailContent);
     }
 
-    public static boolean sendEmail(String mailHost, final String loginAccount, final String loginAuthCode,
+    public static boolean sendEmail(final String mailHost, final String type, final String loginAccount, final String loginAuthCode,
                                     String[] recipients, String mailSubject, String mailContent){
-        return JavaMailUtil.sendEmail(mailHost, loginAccount, loginAuthCode, null, recipients, mailSubject, mailContent);
+        return JavaMailUtil.sendEmail(mailHost, type, loginAccount, loginAuthCode, null, recipients, mailSubject, mailContent);
     }
 
-    public static boolean sendEmail(String mailHost, final String loginAccount, final String loginAuthCode,
+    public static boolean sendEmail(final String mailHost, final String type, final String loginAccount, final String loginAuthCode,
                                     String recipients, String mailSubject, String mailContent){
-        return JavaMailUtil.sendEmail(mailHost, loginAccount, loginAuthCode, null, recipients, mailSubject, mailContent);
+        return JavaMailUtil.sendEmail(mailHost, type, loginAccount, loginAuthCode, null, recipients, mailSubject, mailContent);
     }
 
 }
--- src/test/java/com/wang/other/TestJavaMail.java
+++ src/test/java/com/wang/other/TestJavaMail.java
@@ -29,24 +29,28 @@ public class TestJavaMail {
         // 收件人电子邮箱
         String to = "wangldsz@sinosoft.com.cn";
         // 发件人电子邮箱
-        String from = "1107224733@qq.com";
+        String from = "wang9264541@outlook.com";
         // 指定发送邮件的主机为 smtp.qq.com
         // QQ 邮件服务器
-        String host = "smtp.qq.com";
+        String host = "smtp-mail.outlook.com";
         // 获取系统属性
         Properties properties = System.getProperties();
         // 设置邮件服务器
         properties.setProperty("mail.smtp.host", host);
+        // properties.setProperty("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
-        MailSSLSocketFactory sf = new MailSSLSocketFactory();
-        sf.setTrustAllHosts(true);
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.ssl.socketFactory", sf);
+        // TLS
+        properties.put("mail.smtp.starttls.enable", "true");
+        // SSL
+        // MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        // sf.setTrustAllHosts(true);
+        // properties.put("mail.smtp.ssl.enable", "true");
+        // properties.put("mail.smtp.ssl.socketFactory", sf);
         // 获取默认session对象
         Session session = Session.getDefaultInstance(properties,new Authenticator(){
             public PasswordAuthentication getPasswordAuthentication() {
                 // 发件人邮件用户名、密码
-                return new PasswordAuthentication("1107224733@qq.com", "");
+                return new PasswordAuthentication("wang9264541@outlook.com", "");
             }
         });
         try{
@@ -71,35 +75,26 @@ public class TestJavaMail {
     @Test
     public void Test2(){
         String mailHost = "smtp.qq.com";
+        String type = "SSL";
         String loginAccount = "1107224733@qq.com";
-        String loginAuthCode = "";
-        String name = "学长";
-        String[] recipients = new String[]{"1107224733@qq.com","wang9264541@outlook.com","wangldsz@sinosoft.com.cn"};
-        String mailSubject = "节日祝福";
-        String mailContent = "节日快乐";
-        JavaMailUtil.sendEmail(mailHost, loginAccount, loginAuthCode, name, recipients, mailSubject, mailContent);
-    }
-
-    @Test
-    public void Test3(){
-        String mailHost = "smtp.qq.com";
-        String loginAccount = "1107224733@qq.com";
-        String loginAuthCode = "";
+        String loginAuthCode = "f";
         String name = "学长";
         String recipients = "1107224733@qq.com";
         String mailSubject = "节日祝福";
         String mailContent = "节日快乐";
-        JavaMailUtil.sendEmail(mailHost, loginAccount, loginAuthCode, name, recipients, mailSubject, mailContent);
+        JavaMailUtil.sendEmail(mailHost, type, loginAccount, loginAuthCode, name, recipients, mailSubject, mailContent);
     }
 
     @Test
-    public void Test4(){
-        String mailHost = "smtp.qq.com";
-        String loginAccount = "1107224733@qq.com";
-        String loginAuthCode = "";
+    public void Test3(){
+        String mailHost = "smtp-mail.outlook.com";
+        String type = "TLS";
+        String loginAccount = "wang9264541@outlook.com";
+        String loginAuthCode = "9";
+        String name = "学长";
         String[] recipients = new String[]{"1107224733@qq.com","wangldsz@sinosoft.com.cn"};
         String mailSubject = "节日祝福";
         String mailContent = "节日快乐";
-        JavaMailUtil.sendEmail(mailHost, loginAccount, loginAuthCode, recipients, mailSubject, mailContent);
+        JavaMailUtil.sendEmail(mailHost, type, loginAccount, loginAuthCode, name, recipients, mailSubject, mailContent);
     }
 }
